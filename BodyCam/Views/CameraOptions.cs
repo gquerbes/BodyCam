9e0373981431a89478f42e2a54a306112d7d5ef9
Gabriel <gquerbes@gmail.com>
2017-08-07 09:06:25 -0400

    Initial commit

---
+++ BodyCam/Views/CameraOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BodyCam.Views
+{
+    public class CameraOptions
+    {
+        public CameraOptions()
+        {
+        }
+    }
+}
---
+++ iOS/CustomRenderers/CameraViewRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AVFoundation;
+using BodyCam.iOS.CustomRenderers;
+using BodyCam.Views;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CameraView), typeof(CameraPreviewRenderer))]
+namespace BodyCam.iOS.CustomRenderers
+{
+
+    public class CameraPreviewRenderer : ViewRenderer<CameraView, UICameraPreview>
+    {
+       
+ 
+    }
+}
